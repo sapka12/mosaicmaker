09316bed4cfcbf5a24773d2f1e79e60e0defe026
sapka12 <sapka12@sapka12-ubuntu>
2015-10-03 23:27:08 +0200

    Introduce Indexer

---
+++ src/main/java/hu/sapka12/mozaik/index/Identifier.java
@@ -0,0 +1,32 @@
+package hu.sapka12.mozaik.index;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class Identifier
+{
+    private final String userId;
+    private final String pictureId;
+
+    public Identifier(String userId, String pictureId)
+    {
+        this.userId = userId;
+        this.pictureId = pictureId;
+    }
+
+    public String getUserId()
+    {
+        return userId;
+    }
+
+    public String getPictureId()
+    {
+        return pictureId;
+    }
+ 
+    @Override
+    public String toString()
+    {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }   
+}
